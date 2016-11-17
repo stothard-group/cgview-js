30b6f9387abe722f0b2cca5a971e567458434354
Jason Grant <jgrantk@gmail.com>
2016-11-17 15:55:44 -0700

    add documentation template

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,20 @@
 # Join src files into one file
 echo "Building CGView.js"
-cat \
-  dev/Viewer.js \
-  dev/Debug.js \
-  dev/Utils.js \
-  # dev/JSVinit.js \
-  > src/CGView.js
+# cat \
+#   dev/Viewer.js \
+#   dev/Debug.js \
+#   dev/Utils.js \
+#   # dev/JSVinit.js \
+#   > src/CGView.js
+
 # Create documentation
-# echo "Building Documentation..."
-# doxx -s dev/ -T docs/ -t JSpectraViewer --template doxx/template.jade
+# jsdoc must be installed
+# To install jsdoc globally:
+# > npm install -g jsdoc
+# Get a better template (use --template option) and create a tutorial (--tutorials)
+# Help:
+#   http://usejsdoc.org/index.html
+#   https://github.com/davidshimjs/jaguarjs-jsdoc
+echo "Building Documentation..."
+jsdoc --configure scripts/jsdoc_conf.json --template scripts/jaguarjs-jsdoc --tutorials demo/ --destination docs --readme README.txt dev/*
 echo "Done!"
