f7cd6711446ebc843159a67303c15789afe14d08
Jason Grant <jason.grant@ualberta.ca>
2019-06-13 16:14:02 -0600

    box model continues...

--- dev/Box.js
+++ dev/Box.js
@@ -84,9 +84,12 @@
     }
 
     set relativeTo(value) {
-      if ( CGV.validate(value, ['map', 'canvas']) ) {
+      if ( CGV.validate(value, ['map', 'canvas']) && this.relativeTo !== value ) {
         this._relativeTo = value;
         // TODO change positions based on current map position
+        if (this.position && typeof this.position !== 'string') {
+          this._convertPosition();
+        }
       }
     }
 
@@ -262,6 +265,7 @@
     }
 
     _validateStringPosition(string) {
+      // FIXME: Waiting to update builder and JSON files
       // const allowedStrings = ['top-left', 'top-center', 'top-right', 'middle-left', 'middle-center', 'middle-right', 'bottom-left', 'bottom-center', 'bottom-right'];
       const allowedStrings = ['upper-left', 'upper-center', 'upper-right', 'middle-left', 'middle-center', 'middle-right', 'lower-left', 'lower-center', 'lower-right'];
       CGV.validate(string, allowedStrings);
@@ -341,72 +345,131 @@
     }
 
     _originOnMapFromString(position) {
-      // const margin = 0;
-      // const canvasWidth = this.canvas.width;
-      // const canvasHeight = this.canvas.height;
-      // const boxWidth = this.width;
-      // const boxHeight = this.height;
-      // let x, y;
-      //
-      // //FIXME: will be changed in BUILDER
-      // position = position.replace('upper', 'top');
-      // position = position.replace('lower', 'bottom');
-      //
-      // if (position === 'top-left') {
-      //   x = margin;
-      //   y = margin;
-      // } else if (position === 'top-center') {
-      //   x = (canvasWidth / 2) - (boxWidth / 2);
-      //   y = margin;
-      // } else if (position === 'top-right') {
-      //   x = canvasWidth - boxWidth - margin;
-      //   y = margin;
-      // } else if (position === 'middle-left') {
-      //   x = margin;
-      //   y = (canvasHeight / 2) - (boxHeight / 2);
-      // } else if (position === 'middle-center') {
-      //   x = (canvasWidth / 2) - (boxWidth / 2);
-      //   y = (canvasHeight / 2) - (boxHeight / 2);
-      // } else if (position === 'middle-right') {
-      //   x = canvasWidth - boxWidth - margin;
-      //   y = (canvasHeight / 2) - (boxHeight / 2);
-      // } else if (position === 'bottom-left') {
-      //   x = margin;
-      //   y = canvasHeight - boxHeight - margin;
-      // } else if (position === 'bottom-center') {
-      //   x = (canvasWidth / 2) - (boxWidth / 2);
-      //   y = canvasHeight - boxHeight - margin;
-      // } else if (position === 'bottom-right') {
-      //   x = canvasWidth - boxWidth - margin;
-      //   y = canvasHeight - boxHeight - margin;
-      // }
-      // return {x, y};
+      const viewer = this.viewer;
+      // FIXME: will be changed in BUILDER
+      position = position.replace('upper', 'top');
+      position = position.replace('lower', 'bottom');
+
+      const [yString, xString] = position.split('-');
+
+      // Anchor
+      const anchor = {};
+      if (yString === 'top') {
+        anchor.yPercent = 0;
+      } else if (yString === 'middle') {
+        anchor.yPercent = 50;
+      } else if (yString === 'bottom') {
+        anchor.yPercent = 100;
+      }
+
+      if (xString === 'left') {
+        anchor.xPercent = 0;
+      } else if (xString === 'center') {
+        anchor.xPercent = 50;
+      } else if (xString === 'right') {
+        anchor.xPercent = 100;
+      }
+
+      let bp, bbOffset;
+      const seqLength = viewer.sequence.length;
+
+      if (viewer.format === 'circular') {
+        // Base pair
+        const lengthFractions = {
+          'middle-center': 0, 'top-right': 1, 'middle-right': 2,
+          'bottom-right': 3, 'bottom-center': 4, 'bottom-left': 5,
+          'middle-left': 6, 'top-left': 7, 'top-center': 8
+        };
+        bp = lengthFractions[position] / 8 * seqLength;
+
+        // bbOffset
+        const centerOffset = viewer.backbone.adjustedCenterOffset;
+        bbOffset = 0;
+        if (['top-center', 'bottom-center'].includes(position)) {
+          bbOffset = 1 / 2 * viewer.height;
+        } else if (['middle-left', 'middle-right'].includes(position)) {
+          bbOffset = 1 / 2 * viewer.width;
+        } else if (['top-left', 'top-right', 'bottom-left', 'bottom-right'].includes(position)) {
+          bbOffset = Math.hypot( (1 / 2 * viewer.height), (1 / 2 * viewer.width) );
+        } else if (position === 'middle-center') {
+          // bbOffset = -viewer.backbone.centerOffset;
+        }
+        bbOffset =  (bbOffset * viewer.zoomFactor) - centerOffset;
+
+      } else if (viewer.format === 'linear') {
+        // Base pair
+        if (xString === 'left') {
+          bp = 1;
+        } else if (xString === 'center') {
+          bp = 0.5 * seqLength;
+        } else if (xString === 'right') {
+          bp = seqLength;
+        }
+
+        // bbOffset
+        if (yString === 'top') {
+          bbOffset = 1 / 2 * viewer.height;
+        } else if (yString === 'middle') {
+          bbOffset = 0;
+        } else if (yString === 'bottom') {
+          bbOffset = -1 / 2 * viewer.height;
+        }
+      }
+
+      // console.log({bp, bbOffset, anchor});
+      return this._originOnMapFromObject({bp, bbOffset, anchor});
     }
 
     // bp, bbOffset, anchor
     _originOnMapFromObject(position) {
       const bp = position.bp;
       const bbOffset = position.bbOffset;
+      const anchor = position.anchor || { xPercent: 50, yPercent: 50 };
 
       const centerOffset = bbOffset + this.viewer.backbone.adjustedCenterOffset;
-
       const point = this.canvas.pointForBp(bp, centerOffset);
 
-      // // boxX and boxY are the weighted point on the box dependent on the offsets
-      // // e.g. 0 xPercent would be 0% box width
-      // // e.g. 50 xPercent would be 50% box width
-      // // e.g. 100 xPercent would be 100% box width
-      // const boxX = this.width * xPercent / 100;
-      // const boxY = this.height * yPercent / 100;
-      //
-      // const canvasX = this.canvas.width * xPercent / 100;
-      // const canvasY = this.canvas.height * yPercent / 100;
-      //
-      // const x = canvasX - boxX;
-      // const y = canvasY - boxY;
-      // return {x, y};
+      const boxX = this.width * anchor.xPercent / 100;
+      const boxY = this.height * anchor.yPercent / 100;
 
-      return point;
+      return {
+        x: point.x - boxX,
+        y: point.y - boxY
+      };
+    }
+
+    _convertPosition() {
+      const viewer = this.viewer;
+      if (typeof this.position === 'string') { return; }
+      const relativeTo = this.relativeTo;
+      const origPosition = this.position;
+      let newPosition = {};
+      const positionKeys = Object.keys(origPosition);
+      if (!origPosition.anchor) {
+        // FIXME: set to 'middle-center' after ceating anchor mehthods
+        newPosition.anchor = {xPercent: 50, yPercent: 50};
+      }
+
+      if (relativeTo === 'map') {
+        if ( CGV.validate(positionKeys, ['xPercent', 'yPercent', 'anchor']) ) {
+          const point = {
+            x: origPosition.xPercent * viewer.width,
+            y: origPosition.yPercent * viewer.height
+          };
+          newPosition.bp = viewer.canvas.bpForPoint(point);
+          newPosition.bbOffset = viewer.layout.centerOffsetForPoint(point) - viewer.backbone.adjustedCenterOffset;
+        }
+      } else if (relativeTo === 'canvas') {
+        if ( CGV.validate(positionKeys, ['bbOffset', 'bp', 'anchor']) ) {
+          const centerOffset = origPosition.bbOffset + viewer.backbone.adjustedCenterOffset;
+          const point = viewer.canvas.pointForBp(origPosition.bp, centerOffset);
+          newPosition.xPercent = point.x / viewer.width * 100;
+          newPosition.yPercent = point.y / viewer.height * 100;
+        }
+      } else {
+        newPosition = 'middle-center';
+      }
+      this.position = newPosition;
     }
 
     refresh() {
