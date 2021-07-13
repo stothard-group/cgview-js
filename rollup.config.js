f97755daa099904292ee12b5183ac1587456ae3d
Jason Grant <jason.grant@ualberta.ca>
2021-07-13 16:49:22 -0600

    Add/Improve JSON & CGViewBuilder tutorials

--- rollup.config.js
+++ rollup.config.js
@@ -6,13 +6,13 @@ export default {
   watch: true,
   external: ['d3'],
   output: [
-    {
-      file: 'dist/cgview.js',
-      format: 'iife',
-      name: 'CGV',
-      globals: {d3: 'd3'},
-      banner: '// +-------------------------------------------------------+\n// |             _____________    ___                      |\n// |            / ____/ ____/ |  / (_)__ _      __         |\n// |           / /   / / __ | | / / / _ \\ | /| / /         |\n// |          / /___/ /_/ / | |/ / /  __/ |/ |/ /          |\n// |          \\____/\\____/  |___/_/\\___/|__/|__/           |\n// +-------------------------------------------------------+\n'
-    },
+    // {
+    //   file: 'dist/cgview.js',
+    //   format: 'iife',
+    //   name: 'CGV',
+    //   globals: {d3: 'd3'},
+    //   banner: '// +-------------------------------------------------------+\n// |             _____________    ___                      |\n// |            / ____/ ____/ |  / (_)__ _      __         |\n// |           / /   / / __ | | / / / _ \\ | /| / /         |\n// |          / /___/ /_/ / | |/ / /  __/ |/ |/ /          |\n// |          \\____/\\____/  |___/_/\\___/|__/|__/           |\n// +-------------------------------------------------------+\n'
+    // },
     {
       file: 'dist/cgview.min.js',
       format: 'iife',
@@ -20,23 +20,23 @@ export default {
       globals: {d3: 'd3'},
       plugins: [terser()]
     },
-    {
-      file: 'dist/cgview.esm.min.js',
-      format: 'es',
-      globals: {d3: 'd3'},
-      plugins: [terser()]
-    },
-    {
-      file: 'dist/cgview.esm.js',
-      globals: {d3: 'd3'},
-      format: 'es',
+    // {
+    //   file: 'dist/cgview.esm.min.js',
+    //   format: 'es',
+    //   globals: {d3: 'd3'},
+    //   plugins: [terser()]
+    // },
+    // {
+    //   file: 'dist/cgview.esm.js',
+    //   globals: {d3: 'd3'},
+    //   format: 'es',
     // },
     // {
     //   file: 'dist/cgview.umd.min.js',
     //   format: 'umd',
     //   name: 'CGV',
     //   plugins: [terser()]
-    }
+    // }
   ],
   plugins: [ json() ]
 };
